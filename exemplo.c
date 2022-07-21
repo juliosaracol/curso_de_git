e995cbef4f9950981cfc75436aa2fc6a8e92b105
Julio <juliosaracol@gmail.com>
2022-07-21 11:52:26 -0300

    adicionando função ola mundo no branch

--- exemplo.c
+++ exemplo.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void ola_mundo(void);
 int main(){
 
-printf("ola mundo seja bem vindo a SAECOMP e o curso de git\n");
+ola_mundo();
 
 return 0;
+
+void ola_mundo(void){
+printf("ola mundo seja bem vindo a SAECOMP e o curso de git\n");
 }
